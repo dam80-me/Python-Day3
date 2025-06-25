ae3ae8cd8353bdf3bac5623adc96a7107c7136ac
dam80-me <malinda_madurawela@yahoo.com>
2025-06-25 03:58:18 -0700

    12th commit

--- ex5.py
+++ ex5.py
@@ -1,7 +1,7 @@
 import os
 path_component1 = "C://Users/Student_15/Python Day3"
 path_component2 = "/tmp"
-file_name = "/monthly_report.pdf"
+file_name = "monthly_report.pdf"
 
 full_path = os.path.join(path_component1, path_component2, file_name)
 print(f"Joined path: {full_path}")
